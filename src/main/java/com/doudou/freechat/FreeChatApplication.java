ef795df1e7d0ba156fa702e831495702dde36983
wh1246935943 <1246935943@qq.com>
2022-12-06 19:02:53 +0800

    init

---
+++ src/main/java/com/doudou/freechat/FreeChatApplication.java
@@ -0,0 +1,13 @@
+package com.doudou.freechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeChatApplication.class, args);
+    }
+
+}
