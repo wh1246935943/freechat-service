1c712a97b6443842d10e8f6c18d51bf9efe225ad
wh1246935943 <1246935943@qq.com>
2022-12-19 18:28:24 +0800

    修改为xml方式

--- src/main/java/com/doudou/freechat/config/SecurityConfig.java
+++ src/main/java/com/doudou/freechat/config/SecurityConfig.java
@@ -23,7 +23,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf() // 由于使用的是JWT，我们这里不需要csrf
+        httpSecurity
+                /**
+                 * 由于使用的是JWT，我们这里不需要csrf
+                 * 这里做一个简单得解释：
+                 * JWT 由三部分组成：头部、载荷和签名
+                 * 由于 JWT 包含了签名，因此 JWT 可以用来验证它的完整性，这就意味着，如果有人想要修改 JWT 中的任何内容，将会导致签名无效，因此服务器就会拒绝该 JWT
+                 * 因此，使用 JWT 可以避免跨站请求伪造 (CSRF) 攻击。由于 JWT 的完整性已被验证，因此服务器可以放心地执行请求。
+                 * 但这不能作为唯一得安全措施，比如他没法防止用户直接通过接口来请求，
+                 */
+                .csrf()
                 .disable()
                 .sessionManagement() // 基于token，所以不需要session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/java/com/doudou/freechat/mapper/UserMapper.java
+++ src/main/java/com/doudou/freechat/mapper/UserMapper.java
@@ -2,15 +2,16 @@ package com.doudou.freechat.mapper;
 
 import com.doudou.freechat.dao.UserDao;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM user WHERE id = #{id}")
-    UserDao getUserInfoById(Long id);
+    // @Select("SELECT * FROM user WHERE id = #{id}")
+    UserDao getUserInfoById(@Param("id") Long id);
 
-    @Select("SELECT * FROM user WHERE userName = #{userName}")
+    // @Select("SELECT * FROM user WHERE userName = #{userName}")
     UserDao getUserInfoByName(String userName);
 
 }
